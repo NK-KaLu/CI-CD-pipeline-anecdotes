82dd1a708c280a95eccc93dadff6016a64c2e39a
Niko Kallio <niko.kallio99@outlook.com>
2024-01-24 03:32:40 +0200

    adsasdasdsf

--- cypress/e2e/anecdote.cy.js
+++ cypress/e2e/anecdote.cy.js
@@ -1,6 +1,6 @@
 describe('anecdote app', function() {
   it('front page can be opened', function() {
-    cy.visit('http://localhost:3000')
+    cy.visit('http://localhost:5000')
     cy.contains('Anecdotes')
     cy.contains('create new')
   })

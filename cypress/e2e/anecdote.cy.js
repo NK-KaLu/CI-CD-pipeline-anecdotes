001faf59a67abeeb9a71cecc3117eecf328c2965
Niko Kallio <niko.kallio99@outlook.com>
2024-01-24 07:55:01 +0200

    asdf

--- cypress/e2e/anecdote.cy.js
+++ cypress/e2e/anecdote.cy.js
@@ -1,15 +1,16 @@
+/* eslint-disable no-undef */
 describe('anecdote app', function () {
-  beforeEach(() => {
-    cy.visit('http://localhost:3000');
-  });
+	beforeEach(() => {
+		cy.visit('http://localhost:3000')
+	})
 
-  it('front page can be opened', { retries: 2 }, () => {
-    cy.contains('Anecdotes').then(() => {
-      cy.log('Found "Anecdotes" element successfully');
-    });
+	it('front page can be opened', { retries: 2 }, () => {
+		cy.contains('Anecdotes').then(() => {
+			cy.log('Found "Anecdotes" element successfully')
+		})
   
-    cy.contains('create new').then(() => {
-      cy.log('Found "create new" element successfully');
-    });
-  });
-});
+		cy.contains('create new').then(() => {
+			cy.log('Found "create new" element successfully')
+		})
+	})
+})

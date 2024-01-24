d0b812a6b75f4933361604d9a5138f841778255e
Niko Kallio <niko.kallio99@outlook.com>
2024-01-24 04:42:15 +0200

    Update anecdote.cy.js

--- cypress/e2e/anecdote.cy.js
+++ cypress/e2e/anecdote.cy.js
@@ -1,7 +1,7 @@
 describe('anecdote app', function() {
   it('front page can be opened', function() {
     cy.visit('http://localhost:3000')
-    cy.contains('Anecdoteyss')
+    cy.contains('Anecdotes')
     cy.contains('create new')
   })
 })

a86b7beadb7e1e2b59bb13c97417c974c88ad642
Niko Kallio <niko.kallio99@outlook.com>
2024-01-24 19:19:22 +0200

    db.json gets own service

--- src/services/anecdotes.js
+++ src/services/anecdotes.js
@@ -1,20 +1,22 @@
 /* eslint-disable import/no-anonymous-default-export */
 import axios from 'axios'
 
-const baseUrl = 'https://anecdotesrender.onrender.com/api'
+const baseUrl = 'https://anecdoteserver.onrender.com/api/anecdotes'
 
 const getAll = async () => {
-	const response = await axios.get(`${baseUrl}/anecdotes`)
+	const response = await axios.get(baseUrl)
 	return response.data
 }
 
 const createNew = async (content) => {
-	const response = await axios.post(`${baseUrl}/anecdotes`, { content })
+	const object = content
+	const response = await axios.post(baseUrl, object)
 	return response.data
 }
 
-const addVote = async (id, content) => {
-	const response = await axios.patch(`${baseUrl}/anecdotes/${id}`, content)
+const addVote = async (content) => {
+	const object = content
+	const response = await axios.patch(`${baseUrl}/${object.id}`, object)
 	return response.data
 }
 

4d10e75b4731ed879254943bfa096d532e2a06a2
Niko Kallio <niko.kallio99@outlook.com>
2024-01-24 06:12:58 +0200

    Update anecdote.cy.js

--- cypress/e2e/anecdote.cy.js
+++ cypress/e2e/anecdote.cy.js
@@ -1,7 +1,18 @@
-describe('anecdote app', function() {
+describe('anecdote app', function () {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000');
+  });
+
   it('front page can be opened', { retries: 2 }, () => {
-    cy.visit('http://localhost:3000')
-    cy.contains('Anecdotes')
-    cy.contains('create new')
-  })
-})
+
+    cy.on('uncaught:exception', (err, runnable) => {
+
+      cy.log('Uncaught exception occurred:', err.message);
+      return false; 
+    });
+
+
+    cy.contains('Anecdotes');
+    cy.contains('create new');
+  });
+});
